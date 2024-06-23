e43943d98f6056c0e5bc5dfc745363d7c137b242
Li2nga <137508531+Li2nga@users.noreply.github.com>
2024-06-23 16:22:33 +0300

    addition

--- src/StageTwo/QuickSorts/QuickSort.java
+++ src/StageTwo/QuickSorts/QuickSort.java
@@ -7,22 +7,29 @@ public record QuickSort<T extends Comparable<T>>(T[] arr) {
     }
 
     private void quicksort(int low, int high) {
+
+        //ломаем рекурсию
         if (low >= high) {
             return;
         }
+
+        //ставим пайвот, сортируем массив, запускаем 2 рекурсии на правую и левую части
         int pivot = partition(low, high);
         quicksort(low, pivot - 1);
         quicksort(pivot + 1, high);
     }
 
     private int partition(int low, int high) {
+
+        //Какой-нибудь способ выбрать пайвот
         int pivotIndex = (low + high) / 2;
-        swap(pivotIndex, high);
+
+        swap(pivotIndex, high); //Свапаем в конец, чтобы свести задачу к разбиению по Ламуту
         int pivotIndexCounter = low;
-        for (int i = low; i < high; i++) {
+        for (int i = low; i < high; i++) { //Пробегаем по всему массиву, нанизывая к пайвоту большие элементы
             if (arr[i].compareTo(arr[high]) <= 0) swap(pivotIndexCounter++, i);
         }
-        swap(pivotIndexCounter, high);
+        swap(pivotIndexCounter, high); //Помещаем пивот на финальное место
         return pivotIndexCounter;
     }
 
